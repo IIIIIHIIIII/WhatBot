8b7c50a345ca2cb019eda314b80705e6e97eae9f
Nikhil Sahani <eh4r0015k@gmail.com>
2015-07-21 11:05:37 +0530

    price

---
+++ price.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import untangle
+import time
+import requests
+
+def getPrice():
+    while True:
+        global ui,iu,du,ud
+        
+        value = untangle.parse("http://www.webservicex.net/CurrencyConvertor.asmx/ConversionRate?FromCurrency=USD&ToCurrency=INR").double.cdata
+        ui = value
+        
+        value = untangle.parse("http://www.webservicex.net/CurrencyConvertor.asmx/ConversionRate?FromCurrency=INR&ToCurrency=USD").double.cdata
+        iu = value
+
+        data = requests.get("https://www.cryptonator.com/api/ticker/doge-usd").json()
+        price = data["ticker"]["price"]
+        du = price
+        
+        data = requests.get("https://www.cryptonator.com/api/ticker/usd-doge").json()
+        price = data["ticker"]["price"]
+        ud = price
+        time.sleep(450)
+        
+ui = ""
+iu = ""
+du = ""
+ud = ""
