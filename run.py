e02fa28b982040256ccad11f2362a86e53afac82
Nikhil Sahani <eh4r0015k@gmail.com>
2015-07-22 12:50:23 +0530

    uses config now

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import price
 import time
 import threading
 from stack import YowsupEchoStack
+from config import config
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -10,6 +11,6 @@ logging.basicConfig(level=logging.DEBUG)
 t =  threading.Thread(target=price.getPrice)
 t.start()
 time.sleep(10)
-credentials = ("", "") # Whatsapp Phone,Password
+credentials = (config["yowsup"]["phone"], config["yowsup"]["password"])
 bot = YowsupEchoStack(credentials)
 bot.start()
